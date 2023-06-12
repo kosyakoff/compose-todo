6085218433b5de37b0c082fe1b8d48126e6d2e44
Alex Sondor <kosyakoff1@gmail.com>
2023-06-12 18:20:44 +0200

    Added Hilt module and deps for database

--- app/src/main/java/test/app/todocompose/MainActivity.kt
+++ app/src/main/java/test/app/todocompose/MainActivity.kt
@@ -3,8 +3,10 @@ package test.app.todocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 import test.app.todocompose.ui.theme.ToDoComposeTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/test/app/todocompose/ToDoApplication.kt
@@ -0,0 +1,7 @@
+package test.app.todocompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ToDoApplication : Application()
