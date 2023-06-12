d32db8ce72a496cc6af23b5e1b6c595b3e47205c
Alex Sondor <kosyakoff1@gmail.com>
2023-06-13 00:05:00 +0200

    Added todo tasks repository

---
+++ app/src/main/java/test/app/todocompose/data/repositories/ToDoRepository.kt
@@ -0,0 +1,27 @@
+package test.app.todocompose.data.repositories
+
+import kotlinx.coroutines.flow.Flow
+import test.app.todocompose.data.ToDoDao
+import test.app.todocompose.data.models.ToDoTask
+import javax.inject.Inject
+
+class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
+
+    val getAllTask: Flow<List<ToDoTask>> = toDoDao.getAllTasks()
+    val sortByLowPriority: Flow<List<ToDoTask>> = toDoDao.sortByLowPriority()
+    val sortByHighPriority: Flow<List<ToDoTask>> = toDoDao.sortByHighPriority()
+
+    fun getSelectedTask(taskId: Int): Flow<ToDoTask> = toDoDao.getSelectedTask(taskId)
+
+    suspend fun addTask(toDoTask: ToDoTask) = toDoDao.addTask(toDoTask)
+
+    suspend fun updateTask(toDoTask: ToDoTask) = toDoDao.updateTask(toDoTask)
+
+    suspend fun deleteTask(toDoTask: ToDoTask) = toDoDao.deleteTask(toDoTask)
+
+    suspend fun deleteAllTasks() = toDoDao.deleteAllTasks()
+
+    fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>> =
+        toDoDao.searchDataBase(searchQuery)
+
+}
