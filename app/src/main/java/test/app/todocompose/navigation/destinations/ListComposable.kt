edf07beeb60f1c902b6cf4a7aa326ccec52d4869
Alex Sondor <kosyakoff1@gmail.com>
2023-06-13 04:39:52 +0200

    Added first draft of layout for task list screen

--- app/src/main/java/test/app/todocompose/navigation/destinations/ListComposable.kt
+++ app/src/main/java/test/app/todocompose/navigation/destinations/ListComposable.kt
@@ -4,6 +4,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import test.app.todocompose.ui.screens.list.ListScreen
 import test.app.todocompose.util.Constants
 
 fun NavGraphBuilder.listComposable(
@@ -15,6 +16,6 @@ fun NavGraphBuilder.listComposable(
             type = NavType.StringType
         })
     ) {
-
+        ListScreen(navigateToTaskScreen = navigateToTaskScreen)
     }
 }
---
+++ app/src/main/java/test/app/todocompose/ui/screens/list/ListScreen.kt
@@ -0,0 +1,43 @@
+package test.app.todocompose.ui.screens.list
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import test.app.todocompose.R
+
+@Composable
+fun ListScreen(
+    navigateToTaskScreen: (Int) -> Unit
+) {
+    Scaffold(
+        content = { values -> values.calculateTopPadding() },
+        floatingActionButton = {
+            ListFab(onFabClicked = navigateToTaskScreen)
+        }
+    )
+}
+
+@Composable
+fun ListFab(
+    onFabClicked: (Int) -> Unit
+) {
+    FloatingActionButton(onClick = { onFabClicked(-1) }) {
+        Icon(
+            imageVector = Icons.Filled.Add,
+            contentDescription = stringResource(R.string.add_button),
+            tint = Color.White
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun ListScreenPreview() {
+    ListScreen(navigateToTaskScreen = {})
+}
